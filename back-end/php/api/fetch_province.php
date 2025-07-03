f3f9f667b5b12bcaf78d21bbe860289651cab85a
Huu Xuan <xuan071104@gmail.com>
2025-07-03 20:25:31 +0700

    update

--- back-end/php/api/fetch_province.php
+++ back-end/php/api/fetch_province.php
@@ -1,12 +1,13 @@
 <?php
     include __DIR__ . '/../connect.php';
 
-    $path = $_GET['path'] ?? ''; //p/1?depth=2
+    $path = $_GET['path'] ?? ''; // /p/1 hoặc /d/1
+    $depth = 2;
 
     // Base URL API gốc
     $baseUrl = 'https://provinces.open-api.vn/api/';
 
-    $url = $baseUrl . $path;
+    $url = $baseUrl . $path . '?depth=' . $depth;
 
     $response = file_get_contents($url);
 
